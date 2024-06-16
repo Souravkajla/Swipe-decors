29222f9c3303c32b11508198325e5eded167293d
Souravkajla <159424805+Souravkajla@users.noreply.github.com>
2024-06-16 14:37:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+document
+  .getElementById("scrolldoor")
+  .addEventListener("click", function doors() {
+    window.scrollTo({
+      top: 720,
+      behavior: "smooth",
+    });
+  });
+document
+  .getElementById("scrollwindow")
+  .addEventListener("click", function windo() {
+    window.scrollTo({
+      top: 1520,
+      behavior: "smooth",
+    });
+  });
+document
+  .getElementById("scrollwater")
+  .addEventListener("click", function water() {
+    window.scrollTo({
+      top: 2320,
+      behavior: "smooth",
+    });
+  });
+document
+  .getElementById("scrollfloor")
+  .addEventListener("click", function floor() {
+    window.scrollTo({
+      top: 3120,
+      behavior: "smooth",
+    });
+  });
+document
+  .getElementById("scrollchair")
+  .addEventListener("click", function chair() {
+    window.scrollTo({
+      top: 3950,
+      behavior: "smooth",
+    });
+  });
